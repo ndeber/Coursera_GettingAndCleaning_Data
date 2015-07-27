3729fc7c33a21b8e663780acc90275b5e48225f7
unknown <n.debernardi@FR-J3HL6R1.criteois.lan>
2015-07-27 16:39:59 +0200

    Project put in another directory

--- Project/UCI Dataset Cleaning.R
+++ Project/UCI Dataset Cleaning.R
@@ -1,5 +1,6 @@
 library(data.table)
 
+## Import all source datasets
 data_train <- read.table("./UCI HAR Dataset/train/x_train.txt")
 activity_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
 subjects_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
@@ -11,31 +12,41 @@ subjects_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 features <- read.table("./UCI HAR Dataset/features.txt")
 
+## Merge Train and Test datasets for measures, activities and subjects
 data_merged <- rbind(data_train,data_test)
 activity_merged <- rbind(activity_train,activity_test) 
-subjects_merged <- rbind(subjects_train,subjects_test)
+subjects_merged <- data.frame(rbind(subjects_train,subjects_test))
+subjects_merged$V1 <- factor(subjects_merged$V1)
 
+##Remove unnecessary sets
 remove(data_test,activity_train,activity_test, subjects_train,subjects_test)
-##add this data_train
 
+## Select required features (all Means and Standard deviations)
 features_needed <- features[grepl("mean()",features[,2])|grepl("std()",features[,2]),]
 data_merged_reduced <- data_merged[,features_needed[,1]] ##Change here##
 
+##Name all measures
 colnames(data_merged_reduced) <- as.vector(features_needed[,2])
 colnames(subjects_merged) <- "Subjects"
 colnames(activity_merged) <- "ActivityNb"
 colnames(activity_labels) <- c("ActivityNb","Activity")
 
+## Combine measurement with activities and subjects names
 data_merged_enriched <- cbind(subjects_merged,activity_merged,data_merged_reduced)
 tidy_dataset<- merge(x=activity_labels,y=data_merged_enriched,by = "ActivityNb")
-tidy_dataset <- data.table(tidy_dataset)
-tidy_dataset[,ActivityNb:=NULL]
+tidy_dataset <- data.frame(tidy_dataset)
+tidy_dataset$ActivityNb<-NULL
 
-tidy_dataset2 <- tidy_dataset[, lapply(.SD, sum, na.rm=TRUE), by=list(Activity,Subjects)]
+## Compute the average of each measurement per Activity and Subject
+tidy_dataset_average <- aggregate(data=tidy_dataset,.~Activity+Subjects,FUN=mean, na.rm=TRUE)
 
+##Clean workspace
 remove(features_needed,data_merged_reduced,data_merged_enriched,subjects_merged,activity_merged,activity_labels,features)
-##add this data_merged
 
+## Describe output
+head(tidy_dataset_average)
+str(tidy_dataset_average)
+summary(tidy_dataset_average)
 
 
 
